116c30a1819a7c0f9f28086a2a9f2cb8522fd83d
Sebastian Krause <sbstnkrause1@gmail.com>
2026-01-03 19:10:54 +0100

    second

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def main():
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    while(1):
+
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        screen.fill("black")
+
+
+        pygame.display.flip()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
